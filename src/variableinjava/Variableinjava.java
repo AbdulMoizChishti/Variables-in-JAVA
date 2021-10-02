a62243f47750d73f9ee8198b40756c41c5a7e4a1
AbdulMoizChishti <moaiz599@gmail.com>
2021-10-03 00:29:20 +0500

    Data Types

---
+++ src/variableinjava/Variableinjava.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package variableinjava;
+
+/**
+ *
+ * @author Abdul Moiz Chishti
+ */
+public class Variableinjava {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+
+        
+       int a=2;
+       double b=2.3;
+       float aw=2.5f;
+       char chr='5';
+       String str= "Moiz";
+       byte bb=2;
+       long cc=-45;
+       short dd=3;
+        
+        
+        System.out.println("int = "+a+ "\nDouble = "+b+ "String = "+str+ " char ="+chr);
+    }
+    
+}
